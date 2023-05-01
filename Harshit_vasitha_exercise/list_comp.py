9e1016cd954437539a2b6b41832cfbe466468a12
Abhishek991926 <abhi991926@gmail.com>
2023-05-01 14:10:34 +0530

    list_comp_exercise

---
+++ Harshit_vasitha_exercise/list_comp.py
@@ -0,0 +1,9 @@
+# using list comp. print square of  natural numbers :
+
+square=[i**2 for i in range(1,int(input("enter the range you want : ")))]
+print(square)
+
+# create a list of negetive numbers :
+
+negative_numbers=[-i for i in range(1,int(input("enter the range you want :")))]
+print(negative_numbers)
